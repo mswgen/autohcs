7ca4874f461872ba9786a5096d29e4bdc4226c25
mswgen <54498184+mswgen@users.noreply.github.com>
2021-07-02 10:32:45 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # autohcs
+
+이 프로그램은 [건강상태 자가진단](https://hcs.eduro.go.kr)을 자동으로 진행합니다.
+
+## 사용 방법
+
+먼저 GitHub 레포에서 Code(Add file 오른쪽의 초록색 버튼)를 클릭하고 Download ZIP을 클릭해 다운로드한 다음 압축을 풀어주세요.
+
+이 프로그램은 Node.js와 Yarn을 사용합니다.
+
+### Node.js 설치
+
+* Windows: https://nodejs.org/en/download 에서 Windows Installer를 산택해 다운로드, 설치해주세요.
+
+* macOS: https://nodejs.org/en/download 에서 macOS Installer를 산택해 다운로드, 설치해주세요.
+
+* Linux: 각 배포판의 패키지 매니저를 사용해주세요.
+
+### Yarn 설치
+
+* Windows: 관리자 권한으로 Powershell을 열어서 `npm i -g yarn`을 실행해주세요.
+
+* macOS/Linux: 루트 권한으로 터미널에서 `npm i -g yarn`을 실행하거나 루트 유저가 없는 경우(Ubuntu, macOS 등) `sudo npm i -g yarn`을 실행해주세요.
+
+## 설정 방법
+
+[여기](./config.txt)를 참고해주세요.
+
+## 실행하기
+
+압축을 푼 디렉토리에서 터미널(Windows의 경우 Powershell)을 실행해주세요.
+
+다운로드 후 처음 실행하는 경우 `yarn`, `yarn build`를 순서대로 입력해주세요.
+
+실행하려면 `yarn start`를 입력해주세요.
---
+++ index.ts
@@ -0,0 +1,69 @@
+import puppeteer from 'puppeteer';
+import config from './config.json';
+if (!config.region || !['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18'].includes(config.region) || !config.grade || !['1', '2', '3', '4', '5'].includes(config.grade) || !config.schoolName || !config.name || !config.birth || config.birth.length != 6 || !config.passwd || config.passwd.length != 4) {
+    console.log("please fill the required information. read config.txt for details.");
+    process.exit(1);
+}
+function newline() {
+    if (process.platform == 'win32') {
+        return '\r\n';
+    } else {
+        return '\n';
+    }
+}
+process.stdout.write(`\x1b[0m[1/6] launching browser ...`);
+puppeteer.launch({
+    headless: true,
+    defaultViewport: {
+        width: 1600,
+        height: 900,
+        isLandscape: true
+    }
+}).then(async browser => {
+    process.stdout.write(`\r\x1b[32m[1/6] successfully launched browser!\x1b[0m${newline()}`);
+    process.stdout.write(`\x1b[0m[2/6] navigating to https://hcs.eduro.go.kr ...`);
+    const page = await browser.newPage();
+    await page.goto('https://hcs.eduro.go.kr');
+    process.stdout.write(`\r\x1b[32m[2/6] successfully navigated to the site!\x1b[0m      ${newline()}`);
+    process.stdout.write(`\x1b[0m[3/6] typing your information ...`);
+    await page.click('#btnConfirm2');
+    await page.waitForTimeout(1500);
+    await page.click('.searchBtn');
+    await page.waitForTimeout(1000);
+    await page.select('#sidolabel', config.region);
+    await page.select('#crseScCode', config.grade);
+    await page.type('#orgname', config.schoolName);
+    await page.click('.searchBtn');
+    await page.waitForTimeout(500);
+    await page.click('.layerSchoolArea > li > a');
+    await page.click('.layerFullBtn');
+    await page.waitForTimeout(500);
+    await page.type('#user_name_input', config.name);
+    await page.type('#birthday_input', config.birth);
+    await page.click('#btnConfirm');
+    await page.waitForTimeout(1500);
+    process.stdout.write(`\r\x1b[32m[3/6] successfully typed your information!\x1b[0m${newline()}`);
+    process.stdout.write(`\x1b[0m[4/6] typing password ...`);
+    await page.type('.input_text_common', config.passwd);
+    await page.click('#btnConfirm');
+    await page.waitForTimeout(2000);
+    process.stdout.write(`\r\x1b[32m[4/6] successfully typed your password!\x1b[0m${newline()}`);
+    process.stdout.write(`\x1b[0m[5/6] completing diagnosics ...`);
+    await page.click('.active > a');
+    await page.waitForTimeout(2000);
+    await page.click('#survey_q1a1');
+    await page.click('#survey_q2a1');
+    await page.click('#survey_q3a1');
+    await page.click('#btnConfirm');
+    await page.waitForTimeout(1000);
+    process.stdout.write(`\r\x1b[32m[5/6] successfully completed diagnosics!\x1b[0m${newline()}`);
+    process.stdout.write(`\x1b[0m[6/6] taking screenshot ...`);
+    const doneTime = new Date();
+    await page.screenshot({
+        path: `./diagnosics-${doneTime.getFullYear()}-${doneTime.getMonth()}-${doneTime.getDate()}.png`,
+        fullPage: true
+    });
+    process.stdout.write(`\r\x1b[32m[6/6] successfully took screenshot!\x1b[0m${newline()}`);
+    process.stdout.write(`\x1b[32mScreenshot saved at ./diagnosics-${doneTime.getFullYear()}-${doneTime.getMonth()}-${doneTime.getDate()}.png\x1b[0m${newline()}`);
+    process.exit();
+});
